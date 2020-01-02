00997cc2fa7cf8fb0d82f9a747c449e6877a5f66
Sanm-ZH <zhxpvip7@gmail.com>
2020-01-02 23:00:27 +0800

    update learn-dir basicData

---
+++ learn-dir/basicDataType.py
@@ -0,0 +1,29 @@
+# 变量和基本结构
+x = 1  # assign variable x the value 1
+y = x + 5  # assign variable y the value of x plus 5
+z = y  # assign variable z the value of y
+print(x, y, z)
+
+# 使用数子
+x = 1  # integer
+x = 1.0  # decimal (floating point)
+print(x, y, z)
+
+# type() 数据类型
+x = 1
+print(type(x))  # outputs: <class 'int'>
+
+x = 1.0
+print(type(x))  # outputs: <class 'float'>
+
+# bool 类型
+x = True
+print(type(x))  # outputs: <class 'bool'>
+
+# 字符串
+x = 'This is a string'
+print(x)  # outputs: This is a string
+print(type(x))  # outputs: <class 'str'>
+y = "This is also a string"
+print(x)
+
