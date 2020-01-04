5dccf266a33259ff73c4e378b53206ec886d5fa0
Sanm-ZH <zhxpvip7@gmail.com>
2020-01-04 18:59:13 +0800

    update calc

--- learn-dir/basicDataType.py
+++ learn-dir/basicDataType.py
@@ -27,3 +27,27 @@ print(type(x))  # outputs: <class 'str'>
 y = "This is also a string"
 print(x)
 
+# 对代码进行注释
+# I am a comment...
+x = 1  # the rest of the line is a comment
+# ... and this is a 3rd comment
+text = "# But this isn't a comment because it's a string literal and in quotes."
+
+# This is a comment
+# that crosses multiple lines
+
+# 读取键盘输入
+name = input('enter your name:')
+print(name)
+
+# input 函数始终返回键入值为字符串
+x = input('Enter a number: ')
+print(type(x))
+
+# 若要讲值转换为整数变量，可以使用int函数
+x = int(input('Enter a number: '))
+print(type(x))
+
+# 将数字转换为字符串
+x = 5
+print('The number is ' + str(x))
---
+++ learn-dir/calc.py
@@ -0,0 +1,4 @@
+x = int(input('输入第一个数字:'))
+y = int(input('输入第一个数字:'))
+result = x + y
+print(str(x) + '+' + str(y) + '=' + str(result))
